b184cd7f34eae7549bccce5ed67f7ee852afa6e5
SunnyValentine <98121273+SunnyDaiko@users.noreply.github.com>
2022-03-09 22:16:47 -0800

    Added swim script with height layers
    Used the three fish as test like you said Jo check it out also updated the sand to the final version

--- BayBingo_/Assets/Scripts/FishSwim.cs
+++ BayBingo_/Assets/Scripts/FishSwim.cs
@@ -6,8 +6,16 @@ public class FishSwim : MonoBehaviour
 {
     [SerializeField]
     float speed = 0f;
+    [SerializeField]
+    public Transform target;
+    public float t;
+    
     private void FixedUpdate()
     {
         transform.position = Vector2.MoveTowards(transform.position, new Vector2(32f, transform.position.y), 1f * speed * Time.deltaTime);
+
+        Vector2 a = transform.position;
+        Vector2 b = target.position;
+        transform.position = Vector2.MoveTowards(a, Vector2.Lerp(a, b, t), speed);
     }
 }
