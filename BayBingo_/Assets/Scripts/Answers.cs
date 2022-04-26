1cac0d7c5d8f17cd7a6054c5b44a208bbab2483d
SunnyValentine <98121273+SunnyDaiko@users.noreply.github.com>
2022-04-25 18:25:55 -0700

    Start Menu Screen and added limmericks and others
    Start Menu is just temporarily place will add to it soon, going to continue working on limmericks

---
+++ BayBingo_/Assets/Scripts/Answers.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Answers : MonoBehaviour
+{
+    //public string LevelToLoad;
+
+    public void Start(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+
+    public void FieldGuide(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+
+    public void Help(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+
+    public void Settings(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
